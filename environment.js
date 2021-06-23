290313c08dcbcc1a66abb3ddf5771277b10c503a
Niks <nik.nikhil97@gmail.com>
2021-06-23 22:56:47 +0530

    Fixed Password Leak

--- environment.js
+++
@@ -1,8 +0,0 @@
-const development = {
-  name: "development",
-  //   PARKER.PEN
-  mongoURI:
-    "mongodb+srv://MainUser:EnvironmentPass@cluster0.ongcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
-
-module.exports = development;
