12a6c3ac5e29f24da8ce6d353b5d8143f4444e62
Niks <nik.nikhil97@gmail.com>
2021-06-23 22:22:31 +0530

    Fixed mongodb error with atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:todo" , {useNewUrlParser:true})
+const mongooseLink =
+  "mongodb+srv://MainUser:Parker@cluster0.ongcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongooseLink, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((response) => {
+    console.log("Successfullly Connected to mongo DB server ", response);
+  })
+  .catch((error) => {
+    console.log("**ERROR while connecting to mongo DB server ::", error);
+  });
 
 const db = mongoose.connection;
-
-db.on('error' , console.error.bind(console , "Error connecting to MongoDB server"));
-
-
-// success
-db.once('open' , function(){
-    console.log("Succesfully connected to MongoDB server");
-})
 
 module.exports = db;
